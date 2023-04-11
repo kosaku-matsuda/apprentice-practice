839ebb2b6c584dfdf66fbacfcd001d2947715433
kosaku-matsuda <kuura0818@gmail.com>
2023-04-12 01:26:35 +0900

    コメントを追加

--- Week1-2/QUEST/Ruby/block.rb
+++ Week1-2/QUEST/Ruby/block.rb
@@ -1,9 +1,13 @@
 names = ['上田', '田仲', '堀田']
 
 def print_names(names)
-  names.each.with_index(1) do |name, i|
+  names.each.with_index(2) do |name, i|
     puts "#{i}. #{name}"
   end
 end
 
 print_names(names)
+
+# 似たものでeach_with_indexというメソッドがあるが
+# +1と記述しないといけない
+# with_index()を使えば引数に入れた数字から強制的に表示してくれる
