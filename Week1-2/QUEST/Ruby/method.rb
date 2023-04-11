280432f91492c970905d609d93b97d1db547cf87
kosaku-matsuda <kuura0818@gmail.com>
2023-04-12 03:59:00 +0900

    method.rbを解答

---
+++ Week1-2/QUEST/Ruby/method.rb
@@ -0,0 +1,23 @@
+# メソッドを自作し使うことができる
+
+# 1. Hello World
+# "Hello World" と標準出力（画面）に出力する関数 hello を定義してください。
+
+def hello
+  puts "Hello World"
+end
+
+hello
+
+# 2. 挨拶
+# 名前を引数に受け取り、与えられた名前の挨拶メッセージを標準出力（画面）に出力する greeting という関数を書きなさい。
+
+
+
+def greeting(name)
+  puts "おはよう、#{name}!"
+end
+
+greeting('渡辺')
+
+
