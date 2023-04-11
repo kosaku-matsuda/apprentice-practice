4a9b79725a44767dd6fd37a011a505cc21e764d1
kosaku-matsuda <kuura0818@gmail.com>
2023-04-12 03:23:28 +0900

    block2.rbを解答

--- Week1-2/QUEST/Ruby/block2.rb
+++ Week1-2/QUEST/Ruby/block2.rb
@@ -1,2 +1,15 @@
 # 2. map
 # 与えられた整数の配列の要素を2乗した配列を返す square 関数を定義してください。なお、map メソッドを使うようにしてください。
+
+def square(numbers)
+  numbers.map { |n| n ** 2 }
+end
+
+# 配列を定義
+numbers = [25, 49, 100]
+
+# 関数を呼び出し
+squared_numbers = square(numbers)
+
+# 結果を出力
+puts squared_numbers
