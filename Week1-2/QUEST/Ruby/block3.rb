1cf343a56a34ea654fab4e0002fc01bb3af7113f
kosaku-matsuda <kuura0818@gmail.com>
2023-04-16 23:30:25 +0900

    解答を修正

--- Week1-2/QUEST/Ruby/block3.rb
+++ Week1-2/QUEST/Ruby/block3.rb
@@ -1,3 +1,10 @@
 # 3. select
 # 整数の配列から偶数を抽出した配列を返す関数 select_even_numbers を作成してください。なお、select メソッドを使うようにしてください。
+numbers = [1, 2, 3, 4, 5, 6]
+
+def select_even_numbers(numbers)
+  puts numbers.select {|num| num.even?}
+end
+
+select_even_numbers(numbers)
 
